db3e5de423621a43ec574c7e4ff0fd21d2b0521c
Alexey_ESENKA_NOTEBOOK <alexerisov@gmail.com>
2021-06-29 14:07:44 +0700

    upd: Create empty layout, add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import './App.css';
+import Box from '@material-ui/core/Box';
+import {Grid, Paper, Typography} from '@material-ui/core';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#90caf9',
+    },
+  },
+});
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: '100vh' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+          <Grid item xs={3}>
+            <Typography>Chat Content</Typography>
+          </Grid>
+
+        </Grid>
+      </ThemeProvider>
   );
 }
 
